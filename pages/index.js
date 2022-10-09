3998c843e0548900b843e5c7e2123343573d09d0
Richard C Wilson <rcwilson88@gmail.com>
2022-10-09 13:55:02 -0400

    Happy Birthday!

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,12 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Amy Hamby's Website</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Happy Birthday, Amy!!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
